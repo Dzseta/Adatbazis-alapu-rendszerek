ac3e4933b33f2fbe034101732c72199482c3744a
Dzseta <49132168+Dzseta@users.noreply.github.com>
2020-05-02 01:23:41 +0200

    Add files via upload
    10 kupon és +5 kiadó, így már van 35 rekord. Már csak 115, mi az? Semmiség. Remélem a git-et nem kell megmutatni. :D

---
+++ TEST_KIADO.sql
@@ -0,0 +1,10 @@
+INSERT INTO TEST.KIADO (NEV, IRANYITOSZAM, VAROS, UTCA, HAZSZAM) VALUES ('Kreatív Kft.', 4444, 'Nagy Város', 'Szép utca', '11/b');
+INSERT INTO TEST.KIADO (NEV, IRANYITOSZAM, VAROS, UTCA, HAZSZAM) VALUES ('Józsi és Józsi Kft.', 6723, 'Szeged', 'Egyik utca', '5/a');
+INSERT INTO TEST.KIADO (NEV, IRANYITOSZAM, VAROS, UTCA, HAZSZAM) VALUES ('Nagyon Hosszú Nevű Kiadó', 2535, 'Alsóbögyörős', 'Rövid utca', '2');
+INSERT INTO TEST.KIADO (NEV, IRANYITOSZAM, VAROS, UTCA, HAZSZAM) VALUES ('Kis Kiadó', 1111, 'Mucsaröcsöge', 'Kis utca', '6');
+INSERT INTO TEST.KIADO (NEV, IRANYITOSZAM, VAROS, UTCA, HAZSZAM) VALUES ('Ki Nem Adó', 3654, 'Kis Város', 'Széles utca', '7');
+INSERT INTO TEST.KIADO (NEV, IRANYITOSZAM, VAROS, UTCA, HAZSZAM) VALUES ('Tóthék Kft.', 6723, 'Szeged', 'Másik utca', '3/c');
+INSERT INTO TEST.KIADO (NEV, IRANYITOSZAM, VAROS, UTCA, HAZSZAM) VALUES ('Olvass Többet Zrt.', 5700, 'Gyula', 'Ferenczy Béni ', '5');
+INSERT INTO TEST.KIADO (NEV, IRANYITOSZAM, VAROS, UTCA, HAZSZAM) VALUES ('Bestsellers Co.', 4020, 'New York', 'Wall Street', '87/d');
+INSERT INTO TEST.KIADO (NEV, IRANYITOSZAM, VAROS, UTCA, HAZSZAM) VALUES ('Legjobbkönyvek Bt.', 4800, 'Geszt', '56-os', '56');
+INSERT INTO TEST.KIADO (NEV, IRANYITOSZAM, VAROS, UTCA, HAZSZAM) VALUES ('Szendrei és Társa Bt.', 5711, 'Gyulavári', 'Újélet', '16');
